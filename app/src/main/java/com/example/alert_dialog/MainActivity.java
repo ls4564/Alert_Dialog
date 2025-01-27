f9585a94c05fb80ecc3523ed3bb645772c7d188c
Lior1254 <liorshemtov2008@gmail.com>
2025-01-28 00:45:45 +0200

    Finish all :)

--- app/src/main/java/com/example/alert_dialog/MainActivity.java
+++ app/src/main/java/com/example/alert_dialog/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.alert_dialog;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -17,9 +20,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Random;
 
+/**
+ * The type Main activity.
+ */
 public class MainActivity extends AppCompatActivity  {
+    /**
+     * The Wall.
+     */
     LinearLayout wall;
+    /**
+     * The Adb.
+     */
     AlertDialog.Builder adb;
+    /**
+     * The Si.
+     */
+    Intent si;
+    /**
+     * The Random.
+     */
     Random random;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +51,9 @@ public class MainActivity extends AppCompatActivity  {
 
     }
 
+    /**
+     * Weddings.
+     */
     public void weddings ()
     {
         /*
@@ -40,6 +62,11 @@ public class MainActivity extends AppCompatActivity  {
         wall = (LinearLayout) findViewById(R.id.wall);
     }
 
+    /**
+     * on click 1
+     *
+     * @param view the view
+     */
     public void btn1_clicked(View view) {
         adb = new AlertDialog.Builder(this);
 
@@ -49,6 +76,11 @@ public class MainActivity extends AppCompatActivity  {
         ad.show();
     }
 
+    /**
+     * on click 2
+     *
+     * @param view the view
+     */
     public void btn2_clicked(View view) {
         adb = new AlertDialog.Builder(this);
 
@@ -59,29 +91,46 @@ public class MainActivity extends AppCompatActivity  {
         ad.show();
     }
 
+    /**
+     * on click 3
+     *
+     * @param view the view
+     */
     public void btn3_clicked(View view) {
         adb = new AlertDialog.Builder(this);
 
         adb.setTitle("Bar");
         adb.setMessage("Bar bat zona!!");
         adb.setIcon(R.drawable.bar);
-        adb.setPositiveButton("Fuck Bar", new DialogInterface.OnClickListener() {
+        adb.setPositiveButton("Exit", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                dialog.cancel();
             }
         });
         AlertDialog ad=adb.create();
         ad.show();
     }
 
+    /**
+     * on click 4
+     *
+     * @param view the view
+     */
     public void btn4_clicked(View view) {
         adb = new AlertDialog.Builder(this);
 
         adb.setTitle("Bar");
         adb.setMessage("Bar bat zona!!");
         adb.setIcon(R.drawable.bar);
-        adb.setPositiveButton("Fuck Bar", new DialogInterface.OnClickListener() {
+        adb.setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+
+            }
+        });
+        adb.setNegativeButton("Bar in Color", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 wall.setBackgroundColor(Color.rgb(random.nextInt(256) + 1, random.nextInt(256) + 1, random.nextInt(256) + 1));
@@ -91,13 +140,30 @@ public class MainActivity extends AppCompatActivity  {
         ad.show();
     }
 
+    /**
+     * on click 5
+     *
+     * @param view the view
+     */
     public void btn5_clicked(View view) {
         adb = new AlertDialog.Builder(this);
 
         adb.setTitle("Bar");
         adb.setMessage("Bar bat zona!!");
         adb.setIcon(R.drawable.bar);
-        adb.setPositiveButton("Fuck Bar", new DialogInterface.OnClickListener() {
+        adb.setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        adb.setNegativeButton("Bar in Color", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                wall.setBackgroundColor(Color.rgb(random.nextInt(256) + 1, random.nextInt(256) + 1, random.nextInt(256) + 1));
+            }
+        });
+        adb.setNeutralButton("Bar in White", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 wall.setBackgroundColor(Color.WHITE);
@@ -108,4 +174,24 @@ public class MainActivity extends AppCompatActivity  {
     }
 
 
+
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected (MenuItem item) {
+        int id = item.getItemId();
+
+
+        if(id == R.id.it2)
+        {
+            si = new Intent(this, MainActivity2.class);
+            startActivity(si);
+        }
+        return true;
+
+    }
 }
